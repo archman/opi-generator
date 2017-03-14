3b3cc01208e8cd984da41a2c54d35c5ff84793c4
Will Rogers <wahrogers@gmail.com>
2017-03-14 17:09:22 +0000

    Rule for PV between two values.

--- cssgen/rules.py
+++ cssgen/rules.py
@@ -13,6 +13,33 @@ class RuleNode(nodes.Node):
         self.rule_node.set('name', 'Rule')
 
 
+class BetweenRuleNode(RuleNode):
+
+    def __init__(self, prop_id, pv, min, max):
+        super(BetweenRuleNode, self).__init__(prop_id)
+        self._pv = pv
+        self._min = min
+        self._max = max
+
+    def render(self, rules_node):
+        super(BetweenRuleNode, self).render(rules_node)
+        pv_node = et.SubElement(self.rule_node, 'pv')
+        pv_node.set('trig', 'true')
+        pv_node.text = self._pv
+        exp_node1 = et.SubElement(self.rule_node, 'exp')
+        exp_node1.set('bool_exp', 'pv0 < {}'.format(self._min))
+        val_node1 = et.SubElement(exp_node1, 'value')
+        val_node1.text = 'false'
+        exp_node2 = et.SubElement(self.rule_node, 'exp')
+        exp_node2.set('bool_exp', 'pv0 > {}'.format(self._max))
+        val_node2 = et.SubElement(exp_node2, 'value')
+        val_node2.text = 'false'
+        exp_node3 = et.SubElement(self.rule_node, 'exp')
+        exp_node3.set('bool_exp', 'true')
+        val_node3 = et.SubElement(exp_node3, 'value')
+        val_node3.text = 'true'
+
+
 class GreaterThanRuleNode(RuleNode):
 
     def __init__(self, prop_id, pv, threshold):
--- test/test_rules.py
+++ test/test_rules.py
@@ -22,3 +22,21 @@ def test_greater_than_rule(widget):
     value_element = exp_elements[0].find('./value')
     assert value_element.text == 'true'
     assert exp_elements[1].attrib['bool_exp'] == 'true'
+
+
+def test_between_rule(widget):
+    widget.rules = nodes.ListNode()
+    widget.rules.add_child(rules.BetweenRuleNode('vis', 'dummy_pv', '0', '5'))
+    widget.assemble()
+    rule_element = widget.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.find('./pv').attrib['trig'] == 'true'
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 3
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 < 0'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[1].attrib['bool_exp'] == 'pv0 > 5'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[2].attrib['bool_exp'] == 'true'
