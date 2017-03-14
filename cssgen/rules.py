92c11b03f7af5566ba6ed5eaff9390a7ee3a831b
Will Rogers <wahrogers@gmail.com>
2017-03-14 16:27:37 +0000

    Restructure rule objects.

--- cssgen/rules.py
+++ cssgen/rules.py
@@ -4,25 +4,32 @@ from cssgen import nodes
 
 class RuleNode(nodes.Node):
 
-    def __init__(self, prop_id, pv, expression):
+    def __init__(self, prop_id):
         self._prop_id = prop_id
-        self._pv = pv
-        self._expression = expression
-
-    @staticmethod
-    def escape_exp(exp):
-        exp = exp.replace('>', '&gt;')
-        exp = exp.replace('<', '&lt;')
-        return exp
 
     def render(self, rules_node):
-        rule_node = et.SubElement(rules_node, 'rule')
-        rule_node.set('prop_id', self._prop_id)
-        rule_node.set('name', 'Rule')
-        pv_node = et.SubElement(rule_node, 'pv')
+        self.rule_node = et.SubElement(rules_node, 'rule')
+        self.rule_node.set('prop_id', self._prop_id)
+        self.rule_node.set('name', 'Rule')
+
+
+class GreaterThanRuleNode(RuleNode):
+
+    def __init__(self, prop_id, pv, threshold):
+        super(GreaterThanRuleNode, self).__init__(prop_id)
+        self._pv = pv
+        self._threshold = threshold
+
+    def render(self, rules_node):
+        super(GreaterThanRuleNode, self).render(rules_node)
+        pv_node = et.SubElement(self.rule_node, 'pv')
         pv_node.set('trig', 'true')
         pv_node.text = self._pv
-        exp_node = et.SubElement(rule_node, 'exp')
-        exp_node.set('bool_exp', self._expression)
-        val_node = et.SubElement(exp_node, 'value')
-        val_node.text = 'true'
+        exp_node1 = et.SubElement(self.rule_node, 'exp')
+        exp_node1.set('bool_exp', 'pv0 > {}'.format(self._threshold))
+        val_node1 = et.SubElement(exp_node1, 'value')
+        val_node1.text = 'true'
+        exp_node2 = et.SubElement(self.rule_node, 'exp')
+        exp_node2.set('bool_exp', 'true')
+        val_node2 = et.SubElement(exp_node2, 'value')
+        val_node2.text = 'false'
--- test/test_rules.py
+++ test/test_rules.py
@@ -9,14 +9,16 @@ def test_empty_RulesNode(widget):
     assert '<rules />' in output
 
 
-def test_simple_rule(widget):
+def test_greater_than_rule(widget):
     widget.rules = nodes.ListNode()
-    widget.rules.add_child(rules.RuleNode('vis', 'dummy_pv', 'pv0 > 0'))
+    widget.rules.add_child(rules.GreaterThanRuleNode('vis', 'dummy_pv', '0'))
     widget.assemble()
     rule_element = widget.get_node().find('./rules/rule')
     assert rule_element.find('./pv').text == 'dummy_pv'
     assert rule_element.find('./pv').attrib['trig'] == 'true'
-    exp_element = rule_element.find('./exp')
-    assert exp_element.attrib['bool_exp'] == 'pv0 > 0'
-    value_element = exp_element.find('./value')
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 > 0'
+    value_element = exp_elements[0].find('./value')
     assert value_element.text == 'true'
+    assert exp_elements[1].attrib['bool_exp'] == 'true'
