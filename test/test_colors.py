cce75b75d2a5254d40a598a7c6b7bdb43e2daba6
Will Rogers <wahrogers@gmail.com>
2017-04-13 15:45:54 +0100

    Simplify colors test.

--- test/test_colors.py
+++ test/test_colors.py
@@ -1,14 +1,11 @@
-from opimodel import widgets, colors
-import xml.etree.ElementTree as et
+from opimodel import colors
 
 
 def test_add_color_to_widget(widget, get_renderer):
-    w = widgets.Widget('id', 0, 0, 0, 0)
     c = colors.Color(0, 0, 0, 'black')
-    w.set_fg_color(c)
-    renderer = get_renderer(w)
+    widget.set_fg_color(c)
+    renderer = get_renderer(widget)
     renderer.assemble()
-    print(et.tostring(renderer.get_node()))
     fg_color_nodes = renderer.get_node().findall('./foreground_color')
     assert len(fg_color_nodes) == 1
     color_nodes = renderer.get_node().findall('./foreground_color/color')
