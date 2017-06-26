5c65124c85081568fad5bb6a77c3895f9c37387d
Nick Battam <nicholas.battam@diamond.ac.uk>
2017-06-26 16:02:16 +0100

    Add Apache license

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='cssgen',
       version='0.0.1',
+      url='https://github.com/dls-controls/cssgen',
+      license='APACHE',
       packages=['renderers', 'opimodel'],
       install_requires=['lxml'])
