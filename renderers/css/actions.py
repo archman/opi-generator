600392ddd83c68fc32109ffce08e6c2382971c1c
Nick Battam <nicholas.battam@diamond.ac.uk>
2017-06-27 14:07:11 +0100

    Correct whitespace issues

--- renderers/css/actions.py
+++ renderers/css/actions.py
@@ -9,9 +9,9 @@ class OpiAction(object):
             self.render_one(actions_node, action_model)
 
     def render_one(self, actions_node, action_model):
-            action_node = et.SubElement(actions_node, 'action')
-            action_node.set('type', action_model._action_type)
-            for key, value in vars(action_model).items():
-                if not key.startswith('_'):
-                    n = et.SubElement(action_node, key)
-                    n.text = str(value)
+        action_node = et.SubElement(actions_node, 'action')
+        action_node.set('type', action_model._action_type)
+        for key, value in vars(action_model).items():
+            if not key.startswith('_'):
+                n = et.SubElement(action_node, key)
+                n.text = str(value)
