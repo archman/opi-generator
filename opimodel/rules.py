33d300267593d6290861700cec754003fc2a1683
Nick Battam <nicholas.battam@diamond.ac.uk>
2017-08-18 11:36:54 +0100

    Add >= and <= options to between rule

--- opimodel/rules.py
+++ opimodel/rules.py
@@ -8,11 +8,15 @@ class Rule(object):
 
 class BetweenRule(Rule):
 
-    def __init__(self, prop_id, pv, min_val, max_val):
+    def __init__(self, prop_id, pv, min_val, max_val,
+             gt_equals=False, lt_equals=False):
+
         super(BetweenRule, self).__init__(prop_id)
         self._pv = pv
         self._min = min_val
         self._max = max_val
+        self._lt_equals = lt_equals
+        self._gt_equals = gt_equals
 
 
 class GreaterThanRule(Rule):
--- test/test_rules.py
+++ test/test_rules.py
@@ -41,3 +41,59 @@ def test_between_rule(widget, get_renderer):
     value_element = exp_elements[1].find('./value')
     assert value_element.text == 'false'
     assert exp_elements[2].attrib['bool_exp'] == 'true'
+
+def test_between_rule_gt_equals(widget, get_renderer):
+    widget.rules = []
+    widget.rules.append(rules.BetweenRule('vis', 'dummy_pv', '0', '5', gt_equals=True))
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.find('./pv').attrib['trig'] == 'true'
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 3
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 < 0'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[1].attrib['bool_exp'] == 'pv0 >= 5'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[2].attrib['bool_exp'] == 'true'
+
+
+def test_between_rule_lt_equals(widget, get_renderer):
+    widget.rules = []
+    widget.rules.append(rules.BetweenRule('vis', 'dummy_pv', '0', '5', lt_equals=True))
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.find('./pv').attrib['trig'] == 'true'
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 3
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 <= 0'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[1].attrib['bool_exp'] == 'pv0 > 5'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[2].attrib['bool_exp'] == 'true'
+
+
+def test_between_rule_gt_equals_and_lt_equals(widget, get_renderer):
+    widget.rules = []
+    widget.rules.append(rules.BetweenRule('vis', 'dummy_pv', '0', '5', gt_equals=True, lt_equals=True))
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.find('./pv').attrib['trig'] == 'true'
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 3
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 <= 0'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[1].attrib['bool_exp'] == 'pv0 >= 5'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'false'
+    assert exp_elements[2].attrib['bool_exp'] == 'true'
