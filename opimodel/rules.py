8eeaf23bb7d3986282a62d92155752142774369a
Nick Battam <nicholas.battam@diamond.ac.uk>
2017-10-19 09:13:43 +0100

    Update rules API to support pvSevr and pvVal in same rule in all Rule types

--- opimodel/rules.py
+++ opimodel/rules.py
@@ -54,7 +54,8 @@ class BetweenRule(Rule):
 
 class GreaterThanRule(Rule):
 
-    def __init__(self, prop_id, pv, threshold, name=None):
+    def __init__(self, prop_id, pv, threshold, name=None,
+                 val=True, false_val=False, sevr_options=None):
         """ Construct an rule setting the specified boolean property
                 - True if pv > threshold
                 - False otherwise
@@ -64,15 +65,22 @@ class GreaterThanRule(Rule):
             pv: Controlling PV
             threshold: Threshold value
             name (optional): Rule Name as displayed in CSS OPIEditor
+            val (optional): PV value to set if pv > threshold, default TRUE
+            false_val (optional): PV value to set if pv <= threshold, default FALSE
+            sevr_options (optional): List of tuples (int, widget value) applied to PV severity
         """
         super(GreaterThanRule, self).__init__(prop_id, name)
         self._pv = pv
         self._threshold = threshold
+        self._true_val = val
+        self._false_val = false_val
+        self._sevr_options = sevr_options
 
 
 class SelectionRule(Rule):
 
-    def __init__(self, prop_id, pv, options, var=PV_VAL, name=None):
+    def __init__(self, prop_id, pv, options=None, var=PV_VAL, name=None,
+                 sevr_options=None, val_options=None, else_val=None):
         """ Simple selection rule setting specified property to one of a
             number of possible values based on the pv value, e.g.:
 
@@ -81,14 +89,36 @@ class SelectionRule(Rule):
             widget.rules.append(
                 rules.SelectionRule('on_color', pv_name, options, var=PV_SEVR))
 
+            Note: the var,options API will be deprecated. Use sevr_options and
+            val_options instead:
+
+            widget.rules.append(
+                rules.SelectionRule('on_color', pv_name, sevr_options=options)
+
+            In the created rule the sevr_options are tested before the
+            val_options
+
         Args:
             prop_id: Widget property to set
             pv: Controlling PV
-            options: List of tuples (value, widget value)
-            var: Variable to use (pv0 for value, pvSev0 for alarm severity)
             name (optional): Rule Name as displayed in CSS OPIEditor
+            options [deprecated]: List of tuples (value, widget value)
+            var [deprecated]: Variable to use (pv0 for value, pvSev0 for alarm severity)
+            val_options (optional): List of tuples (value, widget value) applied to PV value
+            sevr_options (optional): List of tuples (value, widget value) applied to PV severity
+            else_value (optional): widget value to use as an else clause
         """
         super(SelectionRule, self).__init__(prop_id, name)
         self._pv = pv
-        self._options = options
-        self._var = var
+        self._else = else_val
+        # support deprecated options,var API
+        if options is not None:
+            if var == PV_VAL:
+                self._val_options = options
+                self._sevr_options = None
+            else:
+                self._val_options = None
+                self._sevr_options = options
+        else:
+            self._sevr_options = sevr_options
+            self._val_options = val_options
--- test/test_rules.py
+++ test/test_rules.py
@@ -35,6 +35,38 @@ def test_greater_than_rule_default_name(widget, get_renderer):
     assert rule_element.attrib['name'] == 'GreaterThanRule'
 
 
+def test_greater_than_rule_sets_val(widget, get_renderer):
+    widget.add_rule(rules.GreaterThanRule(
+        'image_index', 'dummy_pv', '1', val=99))
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.attrib['prop_id'] == 'image_index'
+    assert rule_element.attrib['name'] == 'GreaterThanRule'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 > 1'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == '99'
+
+
+def test_greater_than_rule_sets_false_val(widget, get_renderer):
+    widget.add_rule(rules.GreaterThanRule(
+        'image_index', 'dummy_pv', '1', val=99, false_val=33))
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.attrib['prop_id'] == 'image_index'
+    assert rule_element.attrib['name'] == 'GreaterThanRule'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[1].attrib['bool_exp'] == 'true'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == '33'
+
+
 def test_between_rule_both_closed_0_lte_x_lte_5(widget, get_renderer):
     widget.add_rule(rules.BetweenRule(
         'vis', 'dummy_pv', '0', '5', name="betweenRule",
@@ -234,3 +266,107 @@ def test_selection_rule_one_color_value(widget, get_renderer):
     assert color_element.attrib['green'] == '128'
     assert color_element.attrib['blue'] == '32'
     assert color_element.attrib['name'] == 'murky green'
+
+
+def test_selection_rule_sets_else_condition_when_specified(widget, get_renderer):
+    col = colors.Color(rgb=(64, 128, 32), name="murky green")
+    red = colors.Color(rgb=(255, 0, 0), name="red")
+    widget.add_rule(rules.SelectionRule(
+        'test_property', 'dummy_pv', val_options=[(1, col)], else_val=red))
+
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.attrib['prop_id'] == 'test_property'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 == 1'
+    assert exp_elements[1].attrib['bool_exp'] == 'true'
+
+    color_element = exp_elements[0].find('./value/color')
+    assert color_element.attrib['red'] == '64'
+    assert color_element.attrib['green'] == '128'
+    assert color_element.attrib['blue'] == '32'
+    assert color_element.attrib['name'] == 'murky green'
+
+    color_element = exp_elements[1].find('./value/color')
+    assert color_element.attrib['red'] == '255'
+    assert color_element.attrib['green'] == '0'
+    assert color_element.attrib['blue'] == '0'
+    assert color_element.attrib['name'] == 'red'
+
+
+def test_selection_rule_two_string_val_options(widget, get_renderer):
+    widget.add_rule(rules.SelectionRule(
+        'test_property', 'dummy_pv', val_options=[('1', 'val_one'), ('2', 'val_two')]))
+
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.attrib['prop_id'] == 'test_property'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[0].attrib['bool_exp'] == 'pv0 == 1'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'val_one'
+
+    assert exp_elements[1].attrib['bool_exp'] == 'pv0 == 2'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'val_two'
+
+
+def test_selection_rule_two_string_sevr_options(widget, get_renderer):
+    widget.add_rule(rules.SelectionRule(
+        'test_property', 'dummy_pv',
+        sevr_options=[('1', 'val_one'), ('2', 'val_two')]))
+
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.attrib['prop_id'] == 'test_property'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 2
+    assert exp_elements[0].attrib['bool_exp'] == 'pvSev0 == 1'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'val_one'
+
+    assert exp_elements[1].attrib['bool_exp'] == 'pvSev0 == 2'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'val_two'
+
+
+def test_selection_rule_sevr_options_before_val_options(widget, get_renderer):
+    widget.add_rule(rules.SelectionRule(
+        'test_property', 'dummy_pv',
+        val_options=[('1', 'val_one'), ('2', 'val_two')],
+        sevr_options=[('-1', 'sevr_one'), ('-2', 'sevr_two')]))
+
+    renderer = get_renderer(widget)
+    renderer.assemble()
+    rule_element = renderer.get_node().find('./rules/rule')
+    assert rule_element.find('./pv').text == 'dummy_pv'
+    assert rule_element.attrib['prop_id'] == 'test_property'
+
+    exp_elements = rule_element.findall('./exp')
+    assert len(exp_elements) == 4
+    assert exp_elements[0].attrib['bool_exp'] == 'pvSev0 == -1'
+    value_element = exp_elements[0].find('./value')
+    assert value_element.text == 'sevr_one'
+
+    assert exp_elements[1].attrib['bool_exp'] == 'pvSev0 == -2'
+    value_element = exp_elements[1].find('./value')
+    assert value_element.text == 'sevr_two'
+
+    assert exp_elements[2].attrib['bool_exp'] == 'pv0 == 1'
+    value_element = exp_elements[2].find('./value')
+    assert value_element.text == 'val_one'
+
+    assert exp_elements[3].attrib['bool_exp'] == 'pv0 == 2'
+    value_element = exp_elements[3].find('./value')
+    assert value_element.text == 'val_two'
