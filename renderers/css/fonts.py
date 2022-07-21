74aca7f9fe4cce161fab23a09f2988dafd3da95e
Tong Zhang <zhangt@frib.msu.edu>
2022-07-21 10:24:57 -0400

    FIX: Seems fontdata tag should be used for customized font config.

--- renderers/css/fonts.py
+++ renderers/css/fonts.py
@@ -5,7 +5,8 @@ class OpiFont(object):
 
     def render(self, widget_node, tag_name, font_model):
         font_node = et.SubElement(widget_node, tag_name)
-        opifont_node = et.SubElement(font_node, 'opifont.name')
+        # opifont_node = et.SubElement(font_node, 'opifont.name')
+        opifont_node = et.SubElement(font_node, 'fontdata')
         opifont_node.set('fontName', font_model.fontface)
         opifont_node.set('height', str(font_model.size))
         opifont_node.set('style', str(font_model.style))
