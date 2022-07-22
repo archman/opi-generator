c9235ca583b02cf26ef719ecb2403ec4f270ce8f
Tong Zhang <zhangt@frib.msu.edu>
2022-07-22 15:39:15 -0400

    REF: Add BorderStyle enum for all border styles.

--- opimodel/borders.py
+++ opimodel/borders.py
@@ -1,5 +1,20 @@
-NONE_STYLE = 0
-LINE_STYLE = 1
+class BorderStyle:
+    NONE = 0
+    LINE = 1
+    RAISED = 2
+    LOWERED = 3
+    ETCHED = 4
+    RIDGED = 5
+    BUTTON_RAISED = 6
+    BUTTON_PRESSED = 7
+    DOT = 8
+    DASH = 9
+    DASH_DOT = 10
+    DASH_DOT_DOT = 11
+    TITLE_BAR = 12
+    GROUP_BOX = 13
+    ROUND_RECTANGLE_BACKGROUND = 14
+    EMPTY = 15
 
 
 class Border(object):
