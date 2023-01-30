fc978c11dd090015785ad961627893ce11fcc36c
Tong Zhang <zhangt@frib.msu.edu>
2023-01-30 11:15:21 -0500

    REF: Rendering border style and color for GroupingContainer in phoebus.

--- opigen/opimodel/borders.py
+++ opigen/opimodel/borders.py
@@ -1,3 +1,10 @@
+class GroupBoxBorderStyle:
+    # group box border style for phoebus
+    GROUP_BOX = 0
+    TITLE_BAR = 1
+    LINE = 2
+    NONE = 3
+
 # phoebus does not support border style.
 class BorderStyle:
     NONE = 0
@@ -17,6 +24,13 @@ class BorderStyle:
     ROUND_RECTANGLE_BACKGROUND = 14
     EMPTY = 15
 
+# only for phoebus group widget
+BorderStyle_GroupBoxStyle_MAP = {
+    BorderStyle.GROUP_BOX: GroupBoxBorderStyle.GROUP_BOX,
+    BorderStyle.TITLE_BAR: GroupBoxBorderStyle.TITLE_BAR,
+    BorderStyle.LINE: GroupBoxBorderStyle.LINE,
+    BorderStyle.NONE: GroupBoxBorderStyle.NONE
+}
 
 class Border(object):
 
--- opigen/renderers/phoebus/borders.py
+++ opigen/renderers/phoebus/borders.py
@@ -1,3 +1,6 @@
+from opigen.opimodel.borders import BorderStyle_GroupBoxStyle_MAP
+
+
 class OpiBorder(object):
 
     def __init__(self, text_renderer, color_renderer):
@@ -5,8 +8,15 @@ class OpiBorder(object):
         self._color = color_renderer
 
     def render(self, widget_node, _, border_model):
-        self._text.render(widget_node, 'border_alarm_sensitive', border_model.alarm)
-        self._text.render(widget_node, 'border_width', border_model.width)
-        # phoebus does not support border style
-        # self._text.render(widget_node, 'border_style', border_model.style)
-        self._color.render(widget_node, 'border_color', border_model.color)
+        w_type = widget_node.get('type')
+        if w_type == 'group':
+            # support style
+            _style = BorderStyle_GroupBoxStyle_MAP.get(border_model.style, 0)
+            self._text.render(widget_node, 'style', _style)
+            self._color.render(widget_node, 'foreground_color', border_model.color)
+        else:
+            self._text.render(widget_node, 'border_alarm_sensitive', border_model.alarm)
+            self._text.render(widget_node, 'border_width', border_model.width)
+            # phoebus does not support border style
+            # self._text.render(widget_node, 'border_style', border_model.style)
+            self._color.render(widget_node, 'border_color', border_model.color)
