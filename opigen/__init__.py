ef7e474fb97ab8d94727444ab17dadafe7efe550
Tong Zhang <zhangt@frib.msu.edu>
2023-01-31 10:17:07 -0500

    ENH: Support preload font,color definition files.
    Search for font.def, color.def from:
     - current working directory
     - ~/.opigen/
     - otherwise use package deployed one

--- opigen/__init__.py
+++ opigen/__init__.py
@@ -8,3 +8,11 @@ from .opimodel import rules
 from .opimodel import widgets
 
 from .renderers import *
+
+from .config import get_color_def_path
+from .config import get_font_def_path
+
+# load font def
+fonts.parse_font_file(get_font_def_path())
+# load color def
+colors.parse_color_file(get_color_def_path())
