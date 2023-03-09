d37513a17d91f97c981c332ce658e1f9bb90a087
Tong Zhang <zhangt@frib.msu.edu>
2023-03-09 15:41:54 -0500

    MNT: Bump version to 1.0.1.
    - Update project URL.

--- opigen/__init__.py
+++ opigen/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .opimodel import actions
 from .opimodel import borders
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='opigen',
-      version='1.0.0',
-      url='https://github.com/dls-controls/cssgen',
+      version='1.0.1',
+      url='https://github.com/archman/opi-generator',
       license='APACHE',
       packages=[
           'opigen.renderers', 'opigen.renderers.css', 'opigen.opimodel',
