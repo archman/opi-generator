3905361d9f175f8d17515386d40784ef787a137c
Tong Zhang <zhangt@frib.msu.edu>
2023-04-20 14:51:29 -0400

    MNT: Update example.

--- examples/01.py
+++ examples/01.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from opigen import widgets
-from opigen.contrib import TextUpdate
-from opigen.contrib import Display
 from opigen import Renderer
+from opigen import widgets
+from opigen.contrib import TextUpdate, Display
 
 data = [
     ('LS1_CA01:BPM_D1129', 'VA:LS1_CA01:BPM_D1129:X_RD', 'VA:LS1_CA01:BPM_D1129:Y_RD'),
     ('LS1_CA01:BPM_D1144', 'VA:LS1_CA01:BPM_D1144:X_RD', 'VA:LS1_CA01:BPM_D1144:Y_RD')
 ]
 
+# widget position, size controls
 height = 25
 hgap, vgap = 5, 5
 w_name, w_xpos, w_ypos = [200, 60, 60]
@@ -22,20 +22,18 @@ x_xpos = x_name + w_name + hgap
 # ypos column
 x_ypos = x_xpos + w_xpos + hgap
 
-# screen
+# screen as the root node
 screen = Display(800, 600, "BPM Readings")
-
+# place widgets
 for name, xpv, ypv in data:
-    # label
     name_lbl = widgets.Label(x_name, y0, w_name, height, name)
     x_rd_text = TextUpdate(x_xpos, y0, w_xpos, height, xpv)
     y_rd_text = TextUpdate(x_ypos, y0, w_ypos, height, ypv)
-    screen.add_child(name_lbl)
-    screen.add_child(x_rd_text)
-    screen.add_child(y_rd_text)
+    screen.add_children([name_lbl, x_rd_text, y_rd_text])
+    # next row of widgets
     y0 += height + vgap
 
-#
+# render the screen
 r = Renderer(screen)
-r.to_opi("01.opi")
-r.to_bob("01.bob")
+r.to_opi("01.opi") # for CS-Studio
+r.to_bob("01.bob") # for Phoebus
